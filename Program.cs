16440a287d73eea73f17782527b1d16e46239198
Humphrey <humphrey.asante@gmail.com>
2017-11-16 10:56:40 +0300

    HelloWorld

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace C_
+{
+    
+    class Prog1 {
+        public void ConstantDeclarations() {
+            const float pi = 3.1415927f;
+            const int r = 25;
+            Console.WriteLine(pi * r * r);
+        }
+    }
+    
+    
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            const float pi = 3.1415927f;
+            const int r = 25;
+            Console.WriteLine(pi * r * r);
+            
+            string friend = "buffon";
+
+            Console.WriteLine("Hello" + friend);
+        }
+
+       
+
+    }
+}
